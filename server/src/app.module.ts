fd2f8a84ab45652582fe7e29ed42501ed97c1967
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-06-28 23:11:19 +0200

    Add rate limitng

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'user',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'admin',
+        ttl: 1000,
+        limit: 10,
+      }
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Prisma } from '@prisma/client';
+import { Throttle } from '@nestjs/throttler';
 
+@Throttle({ user: { ttl: 1000, limit: 3 } })
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
