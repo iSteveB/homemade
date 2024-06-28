bfdaf7bdaab43475e80cc920bafd93268b075622
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-06-28 20:48:27 +0200

    add Prisma connected to Postgre DB

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,22 +1,15 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Param,
-  Patch,
-  Delete,
-  Query,
-  ParseIntPipe,
-  ValidationPipe,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Prisma } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: Prisma.UserCreateInput) {
+    return this.usersService.create(createUserDto);
+  }
 
   @Get()
   findAll(@Query('role') role?: 'ADMIN' | 'USER' | 'GUEST') {
@@ -24,25 +17,17 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
+  findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
   }
 
-  @Post()
-  create(@Body(ValidationPipe) user: CreateUserDto) {
-    return this.usersService.create(user);
-  }
-
   @Patch(':id')
-  update(
-    @Param('id', ParseIntPipe) id: number,
-    @Body(ValidationPipe) userUpdate: UpdateUserDto,
-  ) {
-    return this.usersService.update(id, userUpdate);
+  update(@Param('id') id: string, @Body() updateUserDto: Prisma.UserUpdateInput) {
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  delete(@Param('id', ParseIntPipe) id: number) {
+  delete(@Param('id') id: string) {
     return this.usersService.delete(id);
   }
 }
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,68 +1,33 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { NotFoundException } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
+
 
 @Injectable()
 export class UsersService {
-  private users = [
-    { id: 1, name: 'John Doe', role: 'USER', email: 'john.doe@example.com' },
-    { id: 2, name: 'Jane Doe', role: 'ADMIN', email: 'jane.doe@example.com' },
-    { id: 3, name: 'Jim Doe', role: 'GUEST', email: 'jim.doe@example.com' },
-    { id: 4, name: 'Sally Doe', role: 'USER', email: 'sally.doe@example.com' },
-    { id: 5, name: 'Bob Doe', role: 'ADMIN', email: 'bob.doe@example.com' },
-    { id: 6, name: 'Tim Doe', role: 'GUEST', email: 'tim.doe@example.com' },
-    { id: 7, name: 'Linda Doe', role: 'USER', email: 'linda.doe@example.com' },
-    { id: 8, name: 'Frank Doe', role: 'ADMIN', email: 'frank.doe@example.com' },
-    { id: 9, name: 'Karen Doe', role: 'GUEST', email: 'karen.doe@example.com' },
-    { id: 10, name: 'Dave Doe', role: 'USER', email: 'dave.doe@example.com' },
-    { id: 11, name: 'Pat Doe', role: 'ADMIN', email: 'pat.doe@example.com' },
-    { id: 12, name: 'Joe Doe', role: 'GUEST', email: 'joe.doe@example.com' },
-  ];
+  constructor(private readonly databaseService: DatabaseService) {}
 
-  findAll(role?: CreateUserDto['role']) {
+  async create(createUserDto: Prisma.UserCreateInput) {
+    return this.databaseService.user.create({ data: createUserDto });
+  }
+
+  async findAll(role?: 'ADMIN' | 'USER' | 'GUEST') {
     if (role) {
-      const users = this.users.filter((user) => user.role === role);
-      if (users.length === 0) {
-        throw new NotFoundException(`Users with role ${role} not found`);
-      }
-      return users;
-    }
-    return this.users;
-  }
-
-  findOne(id: number) {
-    const user = this.users.find((user) => user.id === id);
-
-    if (!user) {
-      throw new NotFoundException(`User #${id} not found`);
+      return this.databaseService.user.findMany({ where: { role } });
     }
 
-    return user;
+    return this.databaseService.user.findMany();
   }
 
-  create(user: CreateUserDto) {
-    const userHighestId = Math.max(...this.users.map((user) => user.id));
-    const newUser = { ...user, id: userHighestId + 1 };
-    this.users.push(newUser);
-
-    return newUser;
+  async findOne(id: string) {
+    return this.databaseService.user.findUnique({ where: { id } });
   }
 
-  update(id: number, userUpdate: UpdateUserDto) {
-    this.users = this.users.map((user) => {
-      if (user.id === id) {
-        return { ...user, ...userUpdate };
-      }
-      return user;
-    });
-    return this.findOne(id);
+  async update(id: string, updateUserDto: Prisma.UserUpdateInput) {
+    return this.databaseService.user.update({ where: { id }, data: updateUserDto });
   }
 
-  delete(id: number) {
-    const deletedUser = this.findOne(id);
-    this.users = this.users.filter((user) => user.id !== id);
-
-    return deletedUser;
+  async delete(id: string) {
+    return this.databaseService.user.delete({ where: { id } });
   }
 }
