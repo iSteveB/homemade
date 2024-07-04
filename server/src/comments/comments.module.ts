a982502358c1bd405009bf8752c3706f4bd8485c
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2024-07-05 01:38:04 +0200

    add Database module

--- server/src/comments/comments.module.ts
+++ server/src/comments/comments.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
-import { DatabaseService } from 'src/database/database.service';
+import {  } from 'src/database/database.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [DatabaseService],
+  imports: [DatabaseModule],
   providers: [CommentsService],
   controllers: [CommentsController]
 })
